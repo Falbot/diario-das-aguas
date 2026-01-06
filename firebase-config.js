fb1e4a9a8d18ca4c834aebd5993e9f703dd0b789
Falbot <falbot@gmail.com>
2026-01-05 20:39:10 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "xxxxx.firebaseapp.com",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx.appspot.com",
-  messagingSenderId: "123",
-  appId: "1:123:web:abc"
+  apiKey: "AIzaSyCX0el34g6t8QgGigp3nGaVnXSAgfoNM7U",
+  authDomain: "diario-das-aguas-unemat.firebaseapp.com",
+  projectId: "diario-das-aguas-unemat",
+  storageBucket: "diario-das-aguas-unemat.firebasestorage.app",
+  messagingSenderId: "260533766070",
+  appId: "1:260533766070:web:4b0603a0d40a5a57ef2317"
 };
-
-firebase.initializeApp(firebaseConfig);
-window.db = firebase.firestore();
